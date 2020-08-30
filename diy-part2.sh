0af61a931f37cf09d7d3c81f7d6e853997000848
Comair_jl <19809896+KKKKjl@users.noreply.github.com>
2020-08-30 11:19:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,10 +12,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 更改默认主题为Argon
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 echo 'CONFIG_PACKAGE_luci-theme-argon=y' >> .config
 sed -i '/^CONFIG_PACKAGE_luci-theme-bootstrap=y$/d' .config
 
+# openclash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/lean/OpenClash
 echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
